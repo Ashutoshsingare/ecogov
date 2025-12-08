4c42824e905488447dfaa2de14291b9c79027104
Ashutosh singare <ashutoshsingare05@gmail.com>
2025-12-08 10:43:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9998123701-a4ade",
+  "appId": "1:411733588798:web:9f6f3188612f2eb4244a6f",
+  "apiKey": "AIzaSyDscWvMUeQBd6attDya8-MmyLR7Rygoe2U",
+  "authDomain": "studio-9998123701-a4ade.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "411733588798"
+};
